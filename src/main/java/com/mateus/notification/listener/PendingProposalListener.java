106b36b33588acee4e14098df922dbbfdfc9175c
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-02 18:50:39 -0400

    adjusting notify method

--- src/main/java/com/mateus/notification/listener/PendingProposalListener.java
+++ src/main/java/com/mateus/notification/listener/PendingProposalListener.java
@@ -4,16 +4,18 @@ import com.mateus.notification.constant.ConstantMessage;
 import com.mateus.notification.domain.Proposals;
 import com.mateus.notification.service.SnsNotificationService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PendingProposalListener {
 
+    @Autowired
     private SnsNotificationService snsNotificationService;
 
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
     public void pendingProposal(Proposals proposals) {
         String message = String.format(ConstantMessage.PROPOSAL_UNDER_ANALYSIS, proposals.getUsuario().getNome());
-        snsNotificationService.notify(message);
+        snsNotificationService.notify(proposals.getUsuario().getTelefone(), message);
     }
 }
--- src/main/java/com/mateus/notification/service/SnsNotificationService.java
+++ src/main/java/com/mateus/notification/service/SnsNotificationService.java
@@ -1,8 +1,18 @@
 package com.mateus.notification.service;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SnsNotificationService {
 
-    public void notify(String message) {
+    @Autowired
+    private AmazonSNS amazonSNS;
 
+    public void notify(String phone, String message) {
+        PublishRequest publishRequest = new PublishRequest().withMessage(message).withPhoneNumber(phone);
+        amazonSNS.publish(publishRequest);
     }
 }
