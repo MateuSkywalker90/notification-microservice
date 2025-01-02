cad74d9f90fed3a1980f3b704f52692d6b16c7eb
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-02 18:17:17 -0400

    implementing sns access credentials

--- src/main/java/com/mateus/notification/config/AmazonSnsConfiguration.java
+++ src/main/java/com/mateus/notification/config/AmazonSnsConfiguration.java
@@ -1,6 +1,13 @@
 package com.mateus.notification.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,4 +19,15 @@ public class AmazonSnsConfiguration {
     @Value("${aws.secretKey}")
     private String secretKey;
 
+    @Bean
+    public AWSCredentials awsCredentials() {
+        return new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
+    public AmazonSNS amazonSNS() {
+        return AmazonSNSClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
+                .withRegion(Regions.US_EAST_1).build();
+    }
 }
