fe7a2ad8c2048084d5466aeacc9a7d8fc0b3fb59
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-02 18:08:14 -0400

    created amazon sns configuration class

---
+++ src/main/java/com/mateus/notification/config/AmazonSnsConfiguration.java
@@ -0,0 +1,15 @@
+package com.mateus.notification.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonSnsConfiguration {
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+}
